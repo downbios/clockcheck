4a9dadc6eddb92915b2f56ab3f66da41da489c21
downbios <ian.adson.vieira@gmail.com>
2022-05-17 18:45:02 -0300

    formatando data

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-function realdate()
-{
-    var data= new Date();
-var dia=data.getDate();
-var dia_sem=data.getDay();
-var mes=(data.getMonth()+ 1);
-var ano4=data.getFullYear();
-document.getElementById('realdate').innerHTML=dia_sem+"-"+dia+"/"+"/"+ano4;
-}
-
 /* Real Time Function*/
 function time()
 {
-var today=new Date();
-var h=today.getHours();
-var m=today.getMinutes();
-var s=today.getSeconds();
-document.getElementById('realtime').innerHTML=h+":"+m+":"+s;
+var realdate = new Date();
+document.getElementById('realtime').innerHTML= realdate.toUTCString();
 setTimeout('time()',500);
 }
-document.onload = time();
+window.onload = time();
